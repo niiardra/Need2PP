8780bdd46f18064c1f041a98fed2b1bd2f6e78d6
i861742 <adrian.lam01@sap.com>
2018-03-22 19:48:44 -0700

    added floors template, images, and Siyu's html updates

--- buttonReporter/views.py
+++ buttonReporter/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Floor, Washroom
 
 def index(request):
-    return HttpResponse("button reporter index.")
+##    return HttpResponse("button reporter index.")
+    return render(request, 'buttonReporter/index.html')
 
 def floor(request, floor_id):
-    return HttpResponse("You are looking at the page for floor %s" % floor_id)
+    return render(request, 'buttonReporter/floor.html')
 
 def washroom(request, washroom_id):
     return HttpResponse("This is the page for washroom %s. The Gender is: " % washroom_id)
